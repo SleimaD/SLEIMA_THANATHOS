251ee589f95704f733e1ffb2e5395bf457af54a9
SleimaD <sleima@icloud.com>
2024-01-19 10:21:23 +0100

    adding comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { //eventlistener
     //! Twitter
-    let nbrAboTwitter = 12000;
-    let incrementnbrTwitter = 350;    
+    let nbrAboTwitter = 12000; //number we want to obtain at the end
+    let incrementnbrTwitter = 350;   //incrementation numbers
 
     //! Youtube
-    let nbrAboYoutube = 5000;
-    let incrementnbrYoutube = 150;
+    let nbrAboYoutube = 5000; //number we want to obtain at the end
+    let incrementnbrYoutube = 150; //incrementation numbers
 
     //! Facebook
-    let nbrAboFacebook = 7500;
-    let incrementnbrFacebook = 200; 
+    let nbrAboFacebook = 7500; //number we want to obtain at the end
+    let incrementnbrFacebook = 200;  //incrementation numbers
 
 
-    let lesnbrs = document.querySelectorAll(".numbers");
+    let lesnbrs = document.querySelectorAll(".numbers"); //targeting my numbers elements
 
-    let nbrAboArray = [nbrAboTwitter, nbrAboYoutube, nbrAboFacebook];
+    let nbrAboArray = [nbrAboTwitter, nbrAboYoutube, nbrAboFacebook]; //array to stock my number for my loop
 
-    let incrementnbrArray = [incrementnbrTwitter, incrementnbrYoutube, incrementnbrFacebook];
+    let incrementnbrArray = [incrementnbrTwitter, incrementnbrYoutube, incrementnbrFacebook]; //array to stock my incrementations for my loop
 
     lesnbrs.forEach((element, i) => {
 
-        let nbrdebase = 0;
+        let nbrdebase = 0; //setting at 0
 
         setInterval(() => {
             
-            nbrdebase += incrementnbrArray[i];
-            element.innerText = nbrdebase;
+            nbrdebase += incrementnbrArray[i]; // adding my incrementation to 'nbrdebase'
+            element.innerText = nbrdebase; 
 
             if (nbrdebase >= nbrAboArray[i]) {
                 element.innerText = nbrAboArray[i];   
             }
-        }, 15);
+        }, 15); // 15 milliseconds to run the incrementation
     });
 });
  
